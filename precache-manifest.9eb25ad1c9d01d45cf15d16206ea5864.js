af5666e516306bd73f246d96bfcf01c11b1362cc
Beknazar Dosmatov <dosmatovbeknazar@gmail.com>
2020-10-19 19:33:03 +0600

    Updates

---
+++ precache-manifest.9eb25ad1c9d01d45cf15d16206ea5864.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43429375adb55d236e2955a9ab31f7d4",
+    "url": "/bazkor-samurai/index.html"
+  },
+  {
+    "revision": "d4e245a16de0bc2ae7a3",
+    "url": "/bazkor-samurai/static/css/4.620ef933.chunk.css"
+  },
+  {
+    "revision": "de0beb6ac35e83222bb5",
+    "url": "/bazkor-samurai/static/css/5.6ae214ca.chunk.css"
+  },
+  {
+    "revision": "10d83c199e0c50dddd4f",
+    "url": "/bazkor-samurai/static/css/6.9585656f.chunk.css"
+  },
+  {
+    "revision": "e87766b322d914e611bd",
+    "url": "/bazkor-samurai/static/css/main.79f3bc77.chunk.css"
+  },
+  {
+    "revision": "46e8bf9b2e2e35345276",
+    "url": "/bazkor-samurai/static/js/0.bcf87943.chunk.js"
+  },
+  {
+    "revision": "3b22cf2c8b872547aab8",
+    "url": "/bazkor-samurai/static/js/3.7e7a07ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bazkor-samurai/static/js/3.7e7a07ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e245a16de0bc2ae7a3",
+    "url": "/bazkor-samurai/static/js/4.21c1e315.chunk.js"
+  },
+  {
+    "revision": "de0beb6ac35e83222bb5",
+    "url": "/bazkor-samurai/static/js/5.fae16e05.chunk.js"
+  },
+  {
+    "revision": "10d83c199e0c50dddd4f",
+    "url": "/bazkor-samurai/static/js/6.1885f181.chunk.js"
+  },
+  {
+    "revision": "e87766b322d914e611bd",
+    "url": "/bazkor-samurai/static/js/main.ae750d41.chunk.js"
+  },
+  {
+    "revision": "04c66072703fd966a159",
+    "url": "/bazkor-samurai/static/js/runtime-main.18fab3df.js"
+  },
+  {
+    "revision": "e1070b2660052c8774b72feaef57bb4e",
+    "url": "/bazkor-samurai/static/media/mount.e1070b26.jpg"
+  },
+  {
+    "revision": "e7f12dcf63f6a3bfd59a541589b3ac0c",
+    "url": "/bazkor-samurai/static/media/preloader.e7f12dcf.svg"
+  },
+  {
+    "revision": "1a03fde780bf4dedd73b9039fdf32e8a",
+    "url": "/bazkor-samurai/static/media/user.1a03fde7.png"
+  }
+]);
